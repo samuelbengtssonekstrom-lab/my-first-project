69950f8619f39bdeabdb88d0a925e9b310770eff
Samuel <samuel.bengtsson.ekstrom@gmail.com>
2025-12-09 00:28:49 +0000

    mnist

--- First.py
+++ First.py
@@ -1,9 +1,9 @@
-if __name__ == "__main__":
-    main()
-
 def main():
     print("hello world")
     a = 0
     for i in range(1, 10):
         a = a + i
     print(a)
+
+if __name__ == "__main__":
+    main()
